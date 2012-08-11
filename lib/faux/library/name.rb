953ddc0ec39b2aa857ba324fd334e203eedaceed
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-08-11 15:31:07 -0500

    Completely refactoring the generator methods

--- lib/faux/library/name.rb
+++ lib/faux/library/name.rb
@@ -6,46 +6,58 @@ module Faux::Library::Name
     defaulted: true
   }
 
-  def full_name(*args)
-    extract_options_from DEFAULT, *args
-    first = @options[:first_name] || first_name(*args)
-    middle = @options[:middle_name] || middle_name(*args)
-    last = @options[:last_name] || last_name(*args)
-    [first, middle, last].join(' ')
+  def full_name(options)
+    options = options = {} ? DEFAULTS.dup.merge!(options): DEFAULTS
+    first = options[:first_name] || first_name(options)
+    middle = options[:middle_name] || middle_name(options) || ''
+    last = options[:last_name] || last_name(options)
+    first + ' ' + middle + ' ' + last
   end
 
-  def first_name(*args)
-    extract_options_from DEFAULT, *args
-    male = Faux::Dictionary::Name.male_first_names
-    female = Faux::Dictionary::Name.female_first_names
-    case @options[:gender]
-    when :male || 'male' then male
-    when :female || 'female' then female
-    else
-      [male, female].sample
+  def first_name(options)
+    options = DEFAULTS.dup.merge! options unless options[:defaulted]
+    case options[:gender]
+      when :male then male_first_names
+      when 'male' then male_first_names
+      when 'm' then male_first_names
+
+      when :female then female_first_names
+      when 'female' then female_first_names
+      when 'f' then female_first_names
+
+      else male_first_names + female_first_names
     end.sample
   end
 
-  def last_name(*args)
-    extract_options_from DEFAULT, *args
-    unless @options[:hyphenated]
-      Faux::Dictionary::Name.last_names.sample
+  def last_name(options)
+    options = DEFAULTS.dup.merge! options unless options[:defaulted]
+    unless options[:hyphenated]
+      random_last_name
     else
-      [Faux::Dictionary::Name.last_names.sample,
-        Faux::Dictionary::Name.last_names.sample].join('-')
+      random_last_name + '-' + random_last_name
     end
   end
 
-  def middle_name(*args)
-    extract_options_from DEFAULT, *args
-    if @options[:has_middle] || @options[:full_middle_name]
-      if @options[:full_middle_name]
-        Faux::Dictionary::Name.last_names.sample
-      else
-        Faux::Dictionary::Name.last_names.sample.slice(0) + '.'
-      end
+  def middle_name(options)
+    options = DEFAULTS.dup.merge! options unless options[:defaulted]
+    if options[:full_middle]
+      random_last_name
     else
-      ''
-    end
+      random_last_name[0] + '.'
+    end if options[:has_middle] || options[:full_middle]
+  end
+
+  private
+
+  def male_first_names
+    Faux::Dictionary::Name.male_first_names
+  end
+
+  def female_first_names
+    Faux::Dictionary::Name.female_first_names
+  end
+
+  def random_last_name
+    Faux::Dictionary::Name.last_names.sample
   end
 end
